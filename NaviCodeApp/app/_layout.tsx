f26a2a9ad845ab711168236174eb30e5823baee3
SonJH7 <sjunh02@naver.com>
2025-07-31 11:14:20 +0900

    chore(init2): EOL 설정 및 공통 theme 설정 후 layout에 적용

--- NaviCodeApp/app/_layout.tsx
+++ NaviCodeApp/app/_layout.tsx
@@ -1,10 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider as NavigationThemeProvider } from '@react-navigation/native';
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { theme } from '@/theme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -18,12 +20,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <NavigationThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <EmotionThemeProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </EmotionThemeProvider>
+    </NavigationThemeProvider>
   );
 }
